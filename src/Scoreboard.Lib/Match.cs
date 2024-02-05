e4035f9e05e60dc6cb6703f737f71acb3bf2f46e
kirichoff <41067792+kirichoff@users.noreply.github.com>
2024-02-05 22:21:00 +0100

    added implementation

--- src/Scoreboard.Lib/Match.cs
+++ src/Scoreboard.Lib/Match.cs
@@ -2,19 +2,30 @@ namespace Scoreboard.Lib;
 
 public class Match
 {
-  public Match(DateTime startTime)
-  {
-    StartTime = startTime;
-    Id = Guid.NewGuid().ToString();
-  }
-
   public string Id { get; }
   public DateTime StartTime { get; }
   public int HomeTeamScore { get; private set; }
   public int AwayTeamScore { get; private set; }
 
-  private void UpdateScore(int homeTeamScore, int awayTeamScore)
+  public Match(DateTime startTime)
   {
-    throw new NotImplementedException();
+    StartTime = startTime;
+    Id = Guid.NewGuid().ToString();
+    HomeTeamScore = 0;
+    AwayTeamScore = 0;
+  }
+  public void UpdateScore(int homeTeamScore, int awayTeamScore)
+  {
+    if (homeTeamScore < 0 ) throw new ArgumentOutOfRangeException(nameof(homeTeamScore));
+
+    if (awayTeamScore < 0) throw new ArgumentOutOfRangeException(nameof(awayTeamScore));
+
+    if(homeTeamScore < HomeTeamScore) return;
+
+    if(awayTeamScore < AwayTeamScore) return;
+
+    HomeTeamScore = homeTeamScore;
+
+    AwayTeamScore = awayTeamScore;
   }
 }
--- src/Scoreboard.Lib/Scoreboard.cs
+++ src/Scoreboard.Lib/Scoreboard.cs
@@ -2,26 +2,53 @@
 
 public class Scoreboard
 {
-  public List<Match> Matches { get; private set; }
-
+  public List<Match> Matches { get; }
   public IList<Match> MatchesSummaryList
   {
-    get => throw new NotImplementedException();
+    get => _matchesSummaryList();
   }
 
-  public Match StartNewMatch()
-	{
-		throw new NotImplementedException();
-	}
+  public Scoreboard()
+  {
+    Matches = new List<Match>();
+  }
+
+  public Match StartNewMatch(DateTime startTime = new())
+  {
+    var match = new Match(startTime);
+		Matches.Add(match);
+
+    return match;
+  }
 
   public void UpdateScore(string matchId, int homeTeamScore, int awayTeamScore)
-	{
-		throw new NotImplementedException();
-	}
+  {
+    if (string.IsNullOrEmpty(matchId)) throw new ArgumentNullException(nameof(matchId));
+
+    var match = Matches.FirstOrDefault(m => m.Id == matchId);
+
+    if (match == null) return;
+
+    match.UpdateScore(homeTeamScore,awayTeamScore);
+  }
 
 	public void FinishMatch(string matchId)
-	{
-		throw new NotImplementedException();
-	}
+  {
+    if (string.IsNullOrEmpty(matchId)) throw new ArgumentNullException(nameof(matchId));
+
+    var match = Matches.FirstOrDefault(m => m.Id == matchId);
+
+    if (match is null) return;
+
+    Matches.Remove(match);
+  }
+
+  private IList<Match> _matchesSummaryList()
+  {
+    return Matches.OrderByDescending(match => match.AwayTeamScore + match.HomeTeamScore)
+      .ThenBy(match => match.StartTime)
+      .ToList();
+  }
+
 }
 
